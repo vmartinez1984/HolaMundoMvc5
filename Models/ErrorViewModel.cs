40c3e8a814de5be96b5c698bac344aca8817b953
Víctor Martinez <ahal_tocob@hotmail.com>
2022-08-26 20:36:13 -0500

    First commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace HolaMundo.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
---
+++ README.md
@@ -0,0 +1,7 @@
+# Hola mundo c# MVC5
+
+Este solo es un hola mundo en MVC5 en core 6 para verificar que la plataforma o hosting funciona correctamente.
+
+Víctor Martínez
+
+26/08/2022
